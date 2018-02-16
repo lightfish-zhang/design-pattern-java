d129f475b3dfcbc059c7d491f35308e4305cd1d7
zhangguangyu <zhangguangyu@ainemo.com>
2018-02-16 21:40:01 +0800

    feat: 修改java代码build的方式

--- 01-simple-factory/run.sh
+++ 01-simple-factory/run.sh
@@ -1,5 +1,11 @@
-if [ ! -d "build" ]; then
-  mkdir build
+#!/bin/bash
+fileName=$(basename `pwd`).jar
+if [ -d "build" ]; then
+    rm build -rf
 fi
+mkdir build
 javac -sourcepath src -d build src/Main.java &&
-java -classpath build Main $*
+jar cvfm ${fileName} MANIFEST.MF -C build . &&
+echo -e "\n--------------------------------\n build success \n release ${fileName}\n--------------------------------\n"
+rm build -rf
+java -jar ${fileName} ${*}
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
 # 设计模式总结－java
+
+## note
+
+- 编译环境 Linux java 1.8.0_144
+- 每个子目录下有编译与执行脚本`run.sh`
+
+
+## reference
+
+- 本文代码绝大部分参考于 [design-pattern-java-source-code]，在此基础上写下自己的理解与笔记，在代码结构上，有自己的Linux风格，用命令行来执行与打包，而不是用ide的傻瓜式操作
+
+
+[design-pattern-java-source-code]:https://github.com/quanke/design-pattern-java-source-code
